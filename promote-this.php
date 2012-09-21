9f656e9bfab9520b3439d4e52ccc0749a3c3f178
Bill Cromie <bill@cromie.org>
2012-09-21 16:06:38 -0400

    first iteration. really simple.

---
+++ promote-this.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * @package Promote This
+ * @version 0.1
+ */
+/*
+Plugin Name: Promote This
+Plugin URI: http://headliner.fm
+Description: simple plugin for Headliner.fm
+Author: Bill Cromie
+Version: 0.1
+Author URI: http://headliner.fm/
+*/
+function add_promo_script(){
+
+
+	wp_enqueue_script('promo', plugins_url('promo.js', __FILE__), array('jquery'), '0.1', true);
+}
+
+if ( is_admin() ) {
+add_action( 'admin_enqueue_scripts', 'add_promo_script' );
+}
+?>
