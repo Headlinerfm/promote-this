c70d82285161878e6496c7b112d2098939da53a4
Bill Cromie <bill@cromie.org>
2012-09-27 15:27:05 -0400

    minor updates

--- promo.js
+++ promo.js
@@ -1,2 +1,2 @@
-(function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="http://hl.fm/exchange/javascripts/promote_this_widget.min.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","hl_promote_this"));
+(function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="http://hl.fm/exchange/javascripts/promote_this_widget.min.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","hl_promote_this_widget"));
 
