d72b77538363b311a731f85054e72138a8c2707d
Bill Cromie <bill@cromie.org>
2012-09-27 17:57:34 -0400

    changing the url where we pull the widget code from to include the cdn

--- promo.js
+++ promo.js
@@ -1,2 +1,1 @@
-(function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="http://hl.fm/exchange/javascripts/promote_this_widget.min.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","hl_promote_this_widget"));
-
+(function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="http://cdn-exchange.headliner.fm/exchange/javascripts/promote_this_widget.min.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","hl_promote_this_widget"));
