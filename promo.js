618646f0717a937d283b4415b2f1f0f227caa693
Bill Cromie <bill@cromie.org>
2012-09-27 15:02:05 -0400

    updated for post actions and adding a metabox

--- promo.js
+++ promo.js
@@ -1,17 +1,2 @@
-jQuery('div.misc-pub-section.curtime').after('<div class="misc-pub-section" id="hl"><a href="http://hl.fm/exchange/promote_this" target="_blank" class="hl_promote_this_button" data-message="YOUR MESSAGE HERE"><img src="http://hl.fm/exchange/images/promotethis.png" width="276" height="58" alt="Promote This" style="border: none" /></a><script>(function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="http://hl.fm/exchange/javascripts/promote_this_widget.min.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","hl_promote_this_widget"));</script></div>')
+(function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="http://hl.fm/exchange/javascripts/promote_this_widget.min.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","hl_promote_this_widget"));
 
-function updatePR_link(){
-  var promo_text = "YOUR MESSAGE HERE.";
-  var promo_link = jQuery('#sample-permalink').text();
-  if (promo_link !='undefined') {
-  	var excerpt = jQuery('#excerpt').val();
-  	if (excerpt!="") {
-  		promo_text=excerpt+ " " +promo_link;
-  	}else{
-  		promo_text=promo_link;
-  	}
-  }
-  jQuery('.hl_promote_this_button').attr('data-message',promo_text)
-};
-
-var intervalID = window.setInterval(updatePR_link,100);
--- promote-this.php
+++ promote-this.php
@@ -11,6 +11,9 @@ Author: Bill Cromie
 Version: 0.1
 Author URI: http://headliner.fm/
 */
+
+defined('ABSPATH') or die('You\'re not supposed to be here.');
+
 function add_promo_script(){
 
 	wp_enqueue_script('promo', plugins_url('promo.js', __FILE__), array('jquery'), '0.1', true);
@@ -19,4 +22,22 @@ function add_promo_script(){
 if ( is_admin() ) {
 add_action( 'admin_enqueue_scripts', 'add_promo_script' );
 }
+
+
+add_action('post_row_actions', 'testing', 10, 2);
+function testing($actions,$post){
+	$link = get_permalink( $post->ID);
+	$excerpt = get_the_title($post->ID);
+	$str="";
+	if(strlen($excerpt)>=5){
+		$str=$excerpt . " " . $link;
+	}else{
+		$str=$link;
+	}
+
+	$actions['promote_this'] = '<a href="http:\/\/headliner.fm/exchange/promote_this" target="_blank" class="hl_promote_this_button" data-message="' . urlencode($str) .'">Promote this</a>';
+	return $actions;
+}
+
+
 ?>
