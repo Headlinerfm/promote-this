ebc9cb720cdc303d202c9f467621094ae0d4acd0
Bill Cromie <bill@cromie.org>
2012-09-30 11:58:47 -0400

    trying a different tack to alter the admin notice

--- promote-this.php
+++ promote-this.php
@@ -76,16 +76,42 @@ function promote_this_inner_custom_box( $post ) {
   echo '<a href="http:\/\/headliner.fm/exchange/promote_this" target="_blank" class="hl_promote_this_button" data-message="' . urlencode($str) .'">Promote This</a>';
 }
 
-add_action('pending_to_publish',  'load_promote_notice_on_publish',10, 2);
+// add_action('pending_to_publish',  'load_promote_notice_on_publish',10, 2);
+// add_action('new_to_publish',      'load_promote_notice_on_publish', 10, 2);
+// add_action('draft_to_publish',    'load_promote_notice_on_publish', 10, 2);
+// add_action('pending_to_publish',  'load_promote_notice_on_publish', 10, 2);
+// add_action('future_to_publish',   'load_promote_notice_on_publish', 10, 2);
 
-add_action('new_to_publish',      'load_promote_notice_on_publish', 10, 2);
-add_action('draft_to_publish',    'load_promote_notice_on_publish', 10, 2);
-add_action('pending_to_publish',  'load_promote_notice_on_publish', 10, 2);
-add_action('future_to_publish',   'load_promote_notice_on_publish', 10, 2);
+// function load_promote_notice_on_publish($postID,$post){
+//   add_action('admin_notices', 'display_promote_notice',10, 2);
+// }
 
-function load_promote_notice_on_publish($postID,$post){
-  add_action('admin_notices', 'display_promote_notice',10, 2);
+function codex_book_updated_messages( $messages ) {
+  global $post, $post_ID;
+
+  $promo_str = get_promo_str($post);
+
+  $messages['post'] = array(
+    0 => '', // Unused. Messages start at index 1.
+    1 => sprintf( __('Post updated. <a href="%s">View Post</a>'), esc_url( get_permalink($post_ID) ) ),
+    2 => __('Custom field updated.'),
+    3 => __('Custom field deleted.'),
+    4 => __('Post updated.'),
+    /* translators: %s: date and time of the revision */
+    5 => isset($_GET['revision']) ? sprintf( __('Post restored to revision from %s'), wp_post_revision_title( (int) $_GET['revision'], false ) ) : false,
+    6 => sprintf( __('Post published. <a href="%1$s">View post</a> <a href="http:\/\/headliner.fm/exchange/promote_this" target="_blank" class="hl_promote_this_button" data-message="$2%s">Promote This</a>'), esc_url( get_permalink($post_ID) ),esc_url($promo_str) ),
+    7 => __('Post saved.'),
+    8 => sprintf( __('Post submitted. <a target="_blank" href="%s">Preview post</a>'), esc_url( add_query_arg( 'preview', 'true', get_permalink($post_ID) ) ) ),
+    9 => sprintf( __('Post scheduled for: <strong>%1$s</strong>. <a target="_blank" href="%2$s">Preview post</a>'),
+      // translators: Publish box date format, see http://php.net/date
+      date_i18n( __( 'M j, Y @ G:i' ), strtotime( $post->post_date ) ), esc_url( get_permalink($post_ID) ) ),
+    10 => sprintf( __('Post draft updated. <a target="_blank" href="%s">Preview post</a>'), esc_url( add_query_arg( 'preview', 'true', get_permalink($post_ID) ) ) ),
+  );
+
+  return $messages;
 }
+add_filter( 'post_updated_messages', 'codex_book_updated_messages' );
+
 
 function display_promote_notice($postID,$post){
   echo '<div class="updated">
